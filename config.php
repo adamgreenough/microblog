a458e69a7cfd3fbc77ef09bf7faa45cf9de8daef
Adam Greenough <adam@greenough.me>
2020-03-09 00:49:00 +0000

    🎉 Initial Commit

---
+++ config.php
@@ -0,0 +1,47 @@
+<?php
+return array(	
+
+/* ============================================
+   Blog Settings
+ ============================================ */
+
+// Posts per page
+'posts_per_page' => 4,
+
+// The direct URL to your blog without a trailing slash (eg. https://adgr.dev or https://adgr.dev/blog)
+'blog_url' => 'https://microblog.adamgreenough.me',
+
+// The name of your blog
+'blog_name' => 'Nicholas Demo',
+
+// A short description of your blog
+'blog_description' => 'Welcome to my amazing blog powered by Nicholas',
+
+/* ============================================
+   Front-end Settings
+ ============================================ */
+ 
+// Use the front-end (true) or API-only (false)?
+'use_frontend' => true,
+
+// Front-end theme
+'frontend_theme' => 'default',
+ 
+// Date format
+'date_format' => 'jS F Y',
+
+// Page title seperator
+'title_seperator' => '|',
+
+/* ============================================
+   Advanced Settings
+ ============================================ */
+
+// If your Nicholas installation is not in the web root,
+// enter your folder name here with a preceding slash (eg. /blog)
+'base_url' => '/',
+
+// Prepend year and month to post URLs to help avoid slug conflicts?
+'post_base' => false
+
+);
---
+++ posts/2020-03-08_hello-world.md
@@ -0,0 +1,6 @@
+---
+title: Hello world!
+---
+Hopefully this posts to Twitter. Testing, testing, 123!
+
+%media%/test.png
